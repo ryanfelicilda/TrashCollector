06a096750322f36fdd58fa77aaa268a6df749cf0
ryanfelicilda <ryan.g.felicilda@gmil.com>
2019-10-03 08:27:09 -0500

    Modified CreateRoles method in Startup Class. Added new properties and ForeignKeys in both Customer and Employee Model Class.

--- TrashCollectorProject/Models/Customers.cs
+++ TrashCollectorProject/Models/Customers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,19 +11,41 @@ namespace TrashCollectorProject.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Display(Name = "First Name")]
         public string firstName { get; set; }
+
         [Display(Name = "Last Name")]
         public string lastName { get; set; }
+
         [Display(Name = "Street Address")]
         public string streetAddress { get; set; }
+
         [Display(Name = "City")]
         public string city { get; set; }
+
         [Display(Name = "State")]
         public string state { get; set; }
+
         [Display(Name = "Zip Code")]
-        public string zipCode { get; set; }
+        public int zipCode { get; set; }
+
         [Display(Name = "Balance")]
         public int balance { get; set; }
+
+        [Display(Name = "Extra Pickup Date")]
+        public string extraPickupDate { get; set; }
+
+        [Display(Name = "Pickup Date")]
+        public string oneTimePickupDate { get; set; }
+
+        [Display(Name = "Suspend Pickup Date")]
+        public string suspendPickupDate { get; set; }
+
+        
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public enum DayOfWeek { };
     }
 }
--- TrashCollectorProject/Models/Employees.cs
+++ TrashCollectorProject/Models/Employees.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,11 +11,19 @@ namespace TrashCollectorProject.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Display(Name = "First Name")]
         public string firstName { get; set; }
+
         [Display(Name = "Last Name")]
         public string lastName { get; set; }
+
         [Display(Name = "Zip Code")]
         public string zipCode { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public enum DaysOfTheWeek { };
     }
 }
